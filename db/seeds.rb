a1f75eeed444907b7236615d250eaf4cfb96037e
Vinay Gupta <vinak@kreatio.com>
2015-01-30 17:42:52 +0530

    Removed log file and idea file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,1 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create(email: "vgvinay2@gmail.com", encrypted_password: "$2a$10$zvP2lnI2MU7BNbhG38JeW.cWKmDqt7Jy3gs8tytKkQA...", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 1, current_sign_in_at: "2015-01-29 15:51:47", last_sign_in_at: "2015-01-29 15:51:47", current_sign_in_ip: "127.0.0.1", last_sign_in_ip: "127.0.0.1",  first_name: "admin", last_name: "admin")
